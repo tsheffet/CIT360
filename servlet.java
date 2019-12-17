c5d5f5315dd6c10c94ee23c99417ad4bd7c6e26a
tsheffet <38865807+tsheffet@users.noreply.github.com>
2019-12-16 18:48:20 -0700

    Add files via upload

---
+++ servlet.java
@@ -0,0 +1,25 @@
+package servlet;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class helloWorld  extends HttpServlet {
+
+private String message;
+
+public void init() throws ServletException {
+   
+   message = "Hello World";
+}
+
+public void doGet(HttpServletRequest request, HttpServletResponse response)
+   throws ServletException, IOException {
+  
+   response.setContentType("text/html");
+
+   PrintWriter out = response.getWriter();
+   out.println("<h2>" + message + "</h2>");
+}
+
+}
