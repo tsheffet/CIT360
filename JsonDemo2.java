9242bb61cb059fd60ab49684c0086528dab8f5e6
tsheffet <38865807+tsheffet@users.noreply.github.com>
2019-12-18 00:24:38 -0700

    Add files via upload

---
+++ JsonDemo2.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+
+
+public class JsonDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter your name: ");
+			String name = input.nextLine();
+			
+		System.out.print("Enter your age: ");
+			int age = input.nextInt();
+			
+		System.out.print("Enter if you're a studen: (y/n) ");
+			String student = input.nextLine();
+			
+		//JSON Object
+		JSONObject root = new JSONObect();
+		
+		root.put("name", name);
+
+		JSONObject info = new JSONObect();
+		info.put("age", age);
+		info.put("student", student);
+		
+		System.out.println(root.toJSONString());
+		
+
+	}
+
+}
